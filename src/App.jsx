af64ca9269650e903d8ee97e2862052c11c95930
gmnayem631 <you@example.comgmnayem631@gmail.com>
2025-06-10 11:09:05 +0600

    initializing the TrackBack project and installed necessary packeges

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+import "./App.css";
+
+function App() {
+  return (
+    <>
+      <h1 className="text-text-primary">TrackBack</h1>
+      <button className="btn bg-info">Hello</button>
+    </>
+  );
+}
+
+export default App;
