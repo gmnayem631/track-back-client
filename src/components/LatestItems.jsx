4c9cf252724bf44b017d5f45e71bab2a13eab09e
gmnayem631 <you@example.comgmnayem631@gmail.com>
2025-06-15 11:08:57 +0600

    added search functionality to the latest items page

--- src/components/LatestItems.jsx
+++ src/components/LatestItems.jsx
@@ -1,10 +1,18 @@
 import { useLoaderData } from "react-router";
 import ItemsCard from "./ItemsCard";
 import { IoSearch } from "react-icons/io5";
+import { useState } from "react";
 
 const LatestItems = () => {
   const items = useLoaderData();
+  const [searchTerm, setSearchTerm] = useState("");
 
+  // Filter items by title or location
+  const filteredItems = items.filter(
+    (item) =>
+      item.title.toLowerCase().includes(searchTerm.toLowerCase()) ||
+      item.location.toLowerCase().includes(searchTerm.toLowerCase())
+  );
   return (
     <div className="my-10 max-w-11/12 mx-auto">
       <div className="flex justify-between">
@@ -13,15 +21,16 @@ const LatestItems = () => {
           <input
             className="bg-white py-3 px-14 rounded-full active:border-none"
             type="text"
-            name=""
             placeholder="Enter Title or Location"
+            value={searchTerm}
+            onChange={(e) => setSearchTerm(e.target.value)}
           />
           <IoSearch className="text-3xl" />
         </div>
       </div>
       <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-10 my-8">
-        {items.map((item, index) => (
-          <ItemsCard key={index} item={item}></ItemsCard>
+        {filteredItems.map((item, index) => (
+          <ItemsCard key={index} item={item} />
         ))}
       </div>
     </div>
