82c8b2611a71cdc8d1e25ba711205cc5cc483788
gmnayem631 <you@example.comgmnayem631@gmail.com>
2025-08-10 19:44:52 +0600

    added home button to the error page

--- src/pages/ErrorPage.jsx
+++ src/pages/ErrorPage.jsx
@@ -1,15 +1,29 @@
 import React from "react";
 import errorLottie from "../assets/animations/error-lottie";
 import Lottie from "lottie-react";
+import { useNavigate } from "react-router";
 
 const ErrorPage = () => {
+  const navigate = useNavigate();
+
+  const goHome = () => {
+    navigate("/");
+  };
+
   return (
-    <div className="flex items-center justify-center min-h-screen">
+    <div className="flex flex-col items-center justify-center min-h-screen gap-8 px-4">
       <Lottie
         animationData={errorLottie}
         loop={true}
         className="min-h-96 max-w-96"
-      ></Lottie>
+      />
+      <button
+        onClick={goHome}
+        className="btn btn-neutral px-8 py-3 text-lg"
+        type="button"
+      >
+        Go to Home
+      </button>
     </div>
   );
 };
